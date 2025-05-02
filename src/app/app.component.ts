cc2403c33e85520db612d073dc97b30646aa667b
Jaqueline Valencia Arcila <Confa@A48786.confamiliares.com>
2025-05-02 15:52:45 -0500

    organizar logo y footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   template: '<app-home></app-home>',
   standalone: true,
-  imports: [HomeComponent]
+  imports: [HomeComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'object-counter';
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CameraDetectorComponent } from '../camera-detector/camera-detector.component';
 import { WeightEstimatorComponent } from '../weight-estimator/weight-estimator.component';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [CameraDetectorComponent, WeightEstimatorComponent]
+  imports: [CameraDetectorComponent, WeightEstimatorComponent, CommonModule, FooterComponent]
 })
 export class HomeComponent {
   // No necesitamos lógica adicional aquí por ahora
